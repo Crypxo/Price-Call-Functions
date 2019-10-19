ed233b5c34451649f48bd5a64db282e4ff0fba89
Chance Embrey <chance.embrey@quarkworks.co>
2019-10-18 23:21:13 -0500

    btc price fetch function works in dev - production needs api keys

---
+++ api/bitcoinPrice.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const axios = require('axios');
+const { CC_API_KEY } = process.env;
+
+module.exports = (_req, res) => {
+    const options = {
+        headers: {
+            authorization: CC_API_KEY
+        }
+    }
+    axios.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD", options).then(response => {
+        res.send("BTC PRICE: " + response.data.USD)
+    }).catch(_err => {
+        res.send("ERROR");
+    })
+}
