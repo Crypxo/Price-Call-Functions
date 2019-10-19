eb96a1d86f7cddbc107a6b2ebae208ad606c2b38
Chance Embrey <chance.embrey@quarkworks.co>
2019-10-18 23:48:03 -0500

    adds prettier config and tidies bitcoinPrice file; adds now.json to hold api key as a secret in production

--- api/bitcoinPrice.js
+++ api/bitcoinPrice.js
@@ -1,16 +1,20 @@
-require('dotenv').config();
-const axios = require('axios');
+require("dotenv").config();
+const axios = require("axios");
 const { CC_API_KEY } = process.env;
 
 module.exports = (_req, res) => {
     const options = {
-        headers: {
-            authorization: CC_API_KEY
-        }
-    }
-    axios.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD", options).then(response => {
-        res.send("BTC PRICE: " + response.data.USD)
-    }).catch(_err => {
-        res.send("ERROR");
-    })
-}
+        headers: { authorization: CC_API_KEY },
+    };
+    axios
+        .get(
+            "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD",
+            options
+        )
+        .then(response => {
+            res.send("BTC PRICE: " + response.data.USD);
+        })
+        .catch(_err => {
+            res.send("ERROR");
+        });
+};
